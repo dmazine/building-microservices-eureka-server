622608989ecb326b33fe5ab3b8eb1e5af75a880a
Diego Rani Mazine <diego.mazine@livelo.com.br>
2018-08-04 15:21:20 -0300

    Initial version

---
+++ src/main/java/com/dzone/buildingmicroserviceseurekaserver/BuildingMicroservicesEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.dzone.buildingmicroserviceseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMicroservicesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingMicroservicesEurekaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dzone/buildingmicroserviceseurekaserver/BuildingMicroservicesEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dzone.buildingmicroserviceseurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuildingMicroservicesEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
